3b9961ce6abc574c8e07e3c98ff03fe471c6640f
diana.lafaye@gmail.com <diana.lafaye@gmail.com>
2015-06-23 20:28:01 -0400

    ADSI-49: format the report and consolidate the backbone js

---
+++ WEB_APP/js/drugs_selects.js
@@ -0,0 +1,45 @@
+// We extend the Backbone.Model prototype to build our own
+var drugModel = Backbone.Model.extend({
+  // We can pass it default values.
+	defaults : {
+		name : ''
+	}
+});	
+
+var drugCollection = Backbone.Collection.extend({
+	model : drugModel,
+	url : "http://localhost:8080/api/adsi/1.0/drugs",
+	parse : function(response){
+		var self = this;
+		_.each(response, function(drug){
+			self.add(drug);
+			$("#drugOption").append("<option id='"+drug.name+"'>"+drug.name+"</option>");
+		});
+	}
+});
+
+var myDrugCollection = new drugCollection;
+myDrugCollection.fetch(); // this makes a call to the server and populates the collection based on the response.	
+
+//We extend the Backbone.Model prototype to build our own
+var reactionModel = Backbone.Model.extend({
+  // We can pass it default values.
+	defaults : {
+		preferredTerm : ''
+	}
+});	
+
+var reactionCollection = Backbone.Collection.extend({
+	model : reactionModel,
+	url : "http://localhost:8080/api/adsi/1.0/reactions",
+	parse : function(response){
+		var self = this;
+		_.each(response, function(reaction){
+			self.add(reaction);
+			$("#reactionOption").append("<option id='"+reaction.preferredTerm+"'>"+reaction.preferredTerm+"</option>");
+		});
+	}
+});
+
+var myReactionCollection = new reactionCollection;
+myReactionCollection.fetch(); // this makes a call to the server and populates the collection based on the response.	
