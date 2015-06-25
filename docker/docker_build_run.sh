fc05803e975b74830c987a20947094b5a1b723ae
Martin <weinig@gmail.com>
2015-06-25 08:28:58 -0400

    ADSI-48 Update scripts to stage files for docker before build

--- docker/docker_build_run.sh
+++ docker/docker_build_run.sh
@@ -2,10 +2,16 @@
 pwd
 
 cd apache-wtsadsi
+mkdir htdocs
+cp -r ../../WEB_APP/* htdocs/
 docker build -t wtsintegration/apache-wtsadsi .
+rm -rf htdocs
 
+cd ..
 cd api-wtsadsi
+cp ../../com.wtsintegration/adsi/1.0/adsi-1.0.jar .
 docker build -t wtsintegration/api-wtsadsi .
+rm adsi-1.0.jar
 
 docker run -d -p 8102:8880 --name api_wts_adsi -i -t wtsintegration/apache-wtsadsi
 docker run -d -p 8101:80 --name http_wts_adsi -i -t wtsintegration/apache-wtsadsi
