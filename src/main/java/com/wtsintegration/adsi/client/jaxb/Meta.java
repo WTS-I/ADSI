131c347f65d4f47dae5b1fd359c91008e304b9e9
WTSMark <mark.horwath@wtsintegration.com>
2015-06-29 21:49:43 -0400

    breaking build to test CI feedback on break

--- src/main/java/com/wtsintegration/adsi/client/jaxb/Meta.java
+++ src/main/java/com/wtsintegration/adsi/client/jaxb/Meta.java
@@ -10,7 +10,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Meta {
 
 	@XmlElement
-	private String disclaimer;
+	private Strong disclaimer;
 	@XmlElement
 	private String license;
 	@XmlElement
