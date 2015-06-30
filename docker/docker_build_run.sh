ed2eb2191149795621d04f370b70472a1150773a
Martin <weinig@gmail.com>
2015-06-30 07:21:24 -0400

    ADSI-67 Dont try running new docker image on publish

--- docker/docker_build_run.sh
+++ docker/docker_build_run.sh
@@ -16,6 +16,8 @@ cp ../../target/LICENSE .
 docker build -t wtsintegration/api-wtsadsi:$1 .
 rm adsi-1.0.jar
 
+if [ -z "$1" ]
+  then
 docker run -d -p 8102:8880 --name api_wts_adsi -i -t wtsintegration/api-wtsadsi
 docker run -d -p 8101:80 --name apache_wts_adsi -i -t wtsintegration/apache-wtsadsi
-
+fi
