44da9f2215e6d94b8d9891fabdf13e654cbc0714
Martin <weinig@gmail.com>
2015-06-30 00:17:10 -0400

    ADSI-48 Add ability to build docker image with a tagged version

--- docker/docker_build_run.sh
+++ docker/docker_build_run.sh
@@ -5,13 +5,13 @@ cd docker
 cd apache-wtsadsi
 mkdir htdocs
 cp -r ../../WEB_APP/* htdocs/
-docker build -t wtsintegration/apache-wtsadsi .
+docker build -t wtsintegration/apache-wtsadsi:$1 .
 rm -rf htdocs
 
 cd ..
 cd api-wtsadsi
 cp ../../com.wtsintegration/adsi/1.0/adsi-1.0.jar .
-docker build -t wtsintegration/api-wtsadsi .
+docker build -t wtsintegration/api-wtsadsi:$1 .
 rm adsi-1.0.jar
 
 docker run -d -p 8102:8880 --name api_wts_adsi -i -t wtsintegration/api-wtsadsi
