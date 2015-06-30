83c47be19372e820e25ad2b6da8faa6cc20fc27f
WTSMark <mark.horwath@wtsintegration.com>
2015-06-29 21:57:19 -0400

    Fixing artificial break

--- src/main/java/com/wtsintegration/adsi/client/jaxb/Meta.java
+++ src/main/java/com/wtsintegration/adsi/client/jaxb/Meta.java
@@ -10,7 +10,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Meta {
 
 	@XmlElement
-	private Strong disclaimer;
+	private String disclaimer;
 	@XmlElement
 	private String license;
 	@XmlElement
